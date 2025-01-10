6936192bfabf85bd38a17a59619693f4c7a1e66b
Matheus Hopper Jansen Costa <mhjc@cin.ufpe.br>
2025-01-10 10:44:28 -0300

    FEAT: add base_model abstract class

---
+++ vespa/methods/base_model.py
@@ -0,0 +1,53 @@
+from abc import ABC, abstractmethod
+from typing import Dict, Optional
+import torch
+from torch.nn import Module
+from torch.utils.data import DataLoader
+
+class BaseModel(ABC, Module):
+    def __init__(self):
+        super().__init__()
+
+    @abstractmethod
+    def forward(self, *args, **kwargs):
+        """Perform the forward pass."""
+        pass
+
+    @abstractmethod
+    def train(self, train_dataset, batch_size: int, epochs: int, device: str):
+        """Train the model on the provided dataset."""
+        pass
+
+    @abstractmethod
+    def valid(self, val_dataset, batch_size: int, device: str):
+        """Validate the model and compute metrics."""
+        pass
+
+    @abstractmethod
+    def test(self, test_dataset, batch_size: int, device: str):
+        """Evaluate the model on the test dataset."""
+        pass
+
+    @abstractmethod
+    def save(self, path: str):
+        """Save the model's state and optimizer."""
+        pass
+
+    @abstractmethod
+    def load(self, path: str):
+        """Load the model's state and optimizer from a file."""
+        pass
+
+    @abstractmethod
+    def print_model_summary(self):
+        """Print a summary of the model architecture."""
+        pass
+
+    @abstractmethod
+    def count_trainable_parameters(self) -> int:
+        """Count the number of trainable parameters."""
+        pass
+
+    def custom_collate_fn(self, batch):
+        """Custom collate function for DataLoader."""
+        return tuple(zip(*batch))
--- vespa/methods/rcnn/model.py
+++ vespa/methods/rcnn/model.py
@@ -1,62 +1,34 @@
-from typing import Dict, List, Optional
-
+from typing import List, Dict, Optional
 import torch
-from sklearn.metrics import precision_recall_fscore_support
-from torch.nn import Module
-from torch.optim import SGD, Adagrad, Adam, RMSprop
 from torch.utils.data import DataLoader
+from sklearn.metrics import precision_recall_fscore_support
 from torchvision.models.detection import fasterrcnn_resnet50_fpn_v2
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+from vespa.methods.utils import configure_optimizer, custom_collate_fn
+from vespa.methods.base_model import BaseModel
 
 
-class RCNN(Module):
+class RCNN(BaseModel):
     def __init__(
         self,
         num_classes: int = 9,
         weights: Optional[str] = 'DEFAULT',
         optimizer_name: str = 'adam',
         lr: float = 0.0001,
-        weight_decay: float = 0.0001,
-        **kwargs,
-    ):
-        """
-        Initializes the RCNN model with customizable parameters.
-
-        Args:
-            num_classes (int): Number of target classes
-                                (including background).
-            weights (str, optional): Pre-trained weights to use.
-                                     Defaults to "DEFAULT".
-            optimizer_name (str): Name of the optimizer to use.
-                                  Defaults to "adam".
-            lr (float): Learning rate. Defaults to 0.0001.
-            weight_decay (float): Weight decay (L2 penalty).
-                                  Defaults to 0.0001.
-            **kwargs: Additional keyword arguments for flexibility.
-        """
+        weight_decay: float = 0.0001
+        ):
         super().__init__()
-
-        # Load pre-trained model
         self.model = fasterrcnn_resnet50_fpn_v2(weights=weights)
-
-        # Replace the predictor head
         in_features = self.model.roi_heads.box_predictor.cls_score.in_features
-        self.model.roi_heads.box_predictor = FastRCNNPredictor(
-            in_features, num_classes
-        )
-
-        # Configure optimizer
-        self.optimizer_name = optimizer_name
-        self.lr = lr
-        self.weight_decay = weight_decay
-        self.optimizer = self.configure_optimizer()
+        self.model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+        self.optimizer = configure_optimizer(self.model, optimizer_name, lr, weight_decay)
         self.num_classes = num_classes
 
     def forward(
         self,
         images: List[torch.Tensor],
         targets: Optional[List[Dict[str, torch.Tensor]]] = None,
-    ):  # noqa
+    ):
         """
         Forward pass for the RCNN model.
 
@@ -72,7 +44,7 @@ class RCNN(Module):
         """
         return self.model(images, targets)
 
-    def train_model(  # noqa
+    def train(
         self,
         train_dataset,
         batch_size=0,
@@ -99,7 +71,7 @@ class RCNN(Module):
             train_dataset,
             batch_size=batch_size,
             shuffle=True,
-            collate_fn=self.custom_collate_fn,
+            collate_fn=custom_collate_fn,
             num_workers=4,
             pin_memory=True,
         )
@@ -134,78 +106,7 @@ class RCNN(Module):
                 f'Epoch [{epoch + 1}/{epochs}] - Average Loss: {avg_loss:.4f}'
             )
 
-    def configure_optimizer(self):
-        """
-        Configure the optimizer based on the selected type.
-
-        Returns:
-            torch.optim.Optimizer: Configured optimizer instance.
-        """
-        optimizer_name = self.optimizer_name.lower()
-
-        if optimizer_name == 'adam':
-            return Adam(
-                self.model.parameters(),
-                lr=self.lr,
-                weight_decay=self.weight_decay,
-            )
-        elif optimizer_name == 'sgd':
-            return SGD(
-                self.model.parameters(),
-                lr=self.lr,
-                momentum=0.9,
-                weight_decay=self.weight_decay,
-            )
-        elif optimizer_name == 'adagrad':
-            return Adagrad(
-                self.model.parameters(),
-                lr=self.lr,
-                weight_decay=self.weight_decay,
-            )
-        elif optimizer_name == 'rmsprop':
-            return RMSprop(
-                self.model.parameters(),
-                lr=self.lr,
-                weight_decay=self.weight_decay,
-            )
-        else:
-            raise ValueError(f'Unsupported optimizer: {self.optimizer_name}')
-
-    @torch.no_grad()
-    def evaluate(self, val_dataset, batch_size=4, device='cuda'):
-        """
-        Evaluate the RCNN model.
-
-        Args:
-            val_dataset: Validation dataset.
-            batch_size (int): Batch size. Defaults to 4.
-            device (str): Device to evaluate on ("cuda" or "cpu").
-                          Defaults to "cuda".
-
-        Returns:
-            List[Dict]: List of predictions.
-        """
-        self.model.eval()
-        self.model.to(device)
-
-        val_loader = DataLoader(
-            val_dataset,
-            batch_size=batch_size,
-            shuffle=False,
-            collate_fn=self.custom_collate_fn,
-            num_workers=4,
-            pin_memory=True,
-        )
-
-        predictions = []
-        for images, _ in val_loader:
-            images = [img.to(device) for img in images]  # noqa
-            outputs = self.model(images)
-            predictions.extend(outputs)
-
-        return predictions
-
-    def validate_model(self, val_dataset, batch_size=4, device='cuda'):
+    def valid(self, val_dataset, batch_size: int, device: str):
         """
         Validate the model and compute average loss on the validation set.
 
@@ -225,7 +126,7 @@ class RCNN(Module):
             val_dataset,
             batch_size=batch_size,
             shuffle=False,
-            collate_fn=self.custom_collate_fn,
+            collate_fn=custom_collate_fn,
             num_workers=4,
             pin_memory=True,
         )
@@ -247,92 +148,7 @@ class RCNN(Module):
         print(f'Validation Loss: {avg_val_loss:.4f}')
         return avg_val_loss
 
-    def save_model(self, path: str):
-        """
-        Save the model and optimizer state.
-
-        Args:
-            path (str): Path to save the model.
-        """
-        torch.save(
-            {
-                'model_state_dict': self.model.state_dict(),
-                'optimizer_state_dict': self.optimizer.state_dict(),
-            },
-            path,
-        )
-
-    def freeze_backbone(self):
-        """
-        Freeze the backbone of the model to prevent updates during training.
-        """
-        for param in self.model.backbone.parameters():
-            param.requires_grad = False
-
-    def unfreeze_backbone(self):
-        """
-        Unfreeze the backbone of the model to allow updates during training.
-        """
-        for param in self.model.backbone.parameters():
-            param.requires_grad = True
-
-    def adjust_learning_rate(self, new_lr: float):
-        """
-        Adjust the learning rate of the optimizer.
-
-        Args:
-            new_lr (float): New learning rate value.
-        """
-        for param_group in self.optimizer.param_groups:
-            param_group['lr'] = new_lr
-        print(f'Learning rate adjusted to {new_lr:.6f}')
-
-    def print_model_summary(self):
-        """
-        Print a summary of the model structure.
-        """
-        print(self.model)
-
-    def count_trainable_parameters(self):
-        """
-        Count the number of trainable parameters in the model.
-
-        Returns:
-            int: Number of trainable parameters.
-        """
-        return sum(
-            p.numel() for p in self.model.parameters() if p.requires_grad
-        )  # noqa
-
-    def set_optimizer(
-        self,
-        optimizer_name: str,
-        lr: float = 0.0001,
-        weight_decay: float = 0.0001,
-    ):
-        """
-        Set a new optimizer for the model.
-
-        Args:
-            optimizer_name (str): Name of the optimizer.
-            lr (float): Learning rate. Defaults to 0.0001.
-            weight_decay (float): Weight decay (L2 penalty).
-                                  Defaults to 0.0001.
-        """
-        self.optimizer_name = optimizer_name
-        self.lr = lr
-        self.weight_decay = weight_decay
-        self.optimizer = self.configure_optimizer()
-        print(f'Optimizer set to {optimizer_name} with learning rate {lr:.6f}')
-        
-    def custom_collate_fn(self, batch):
-        """
-        Função de colagem para combinar imagens e anotações.
-        """
-        return tuple(zip(*batch))
-
-    @torch.no_grad()
-    def test_model(self, test_dataset, batch_size=4, device='cuda'):
+    def test(self, test_dataset, batch_size: int, device: str):
         """
         Test the RCNN model and compute evaluation metrics.
 
@@ -353,7 +169,7 @@ class RCNN(Module):
             test_dataset,
             batch_size=batch_size,
             shuffle=False,
-            collate_fn=self.custom_collate_fn,
+            collate_fn=custom_collate_fn,
             num_workers=4,
             pin_memory=True,
         )
@@ -383,3 +199,42 @@ class RCNN(Module):
 
         print(f'Test Metrics: {metrics}')
         return metrics
+
+    def save(self, path: str):
+        torch.save({'model_state_dict': self.model.state_dict(), 'optimizer_state_dict': self.optimizer.state_dict()}, path)
+
+    def load(self, path: str):
+        checkpoint = torch.load(path)
+        self.model.load_state_dict(checkpoint['model_state_dict'])
+        self.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+
+    def print_model_summary(self):
+        print(self.model)
+
+    def count_trainable_parameters(self) -> int:
+        return sum(p.numel() for p in self.model.parameters() if p.requires_grad)
+    
+    def freeze_backbone(self):
+        """
+        Freeze the backbone of the model to prevent updates during training.
+        """
+        for param in self.model.backbone.parameters():
+            param.requires_grad = False
+
+    def unfreeze_backbone(self):
+        """
+        Unfreeze the backbone of the model to allow updates during training.
+        """
+        for param in self.model.backbone.parameters():
+            param.requires_grad = True
+
+    def adjust_learning_rate(self, new_lr: float):
+        """
+        Adjust the learning rate of the optimizer.
+
+        Args:
+            new_lr (float): New learning rate value.
+        """
+        for param_group in self.optimizer.param_groups:
+            param_group['lr'] = new_lr
+        print(f'Learning rate adjusted to {new_lr:.6f}')
