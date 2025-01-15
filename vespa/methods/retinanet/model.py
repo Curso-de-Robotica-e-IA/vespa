99e10683ba03c89a9808ef1df392b84a7d2507c2
jefferson-norberto2 <jmn@cin.ufpe.br>
2025-01-15 16:36:15 -0300

    MERGE Dev changes on current branch

--- vespa/methods/retinanet/model.py
+++ vespa/methods/retinanet/model.py
@@ -118,7 +118,3 @@ class RetinaNet(Module):
             )
         else:
             raise ValueError('Invalid optimizer')
-        )
-    
-    def forward(self, x):
-        return self.model(x)
