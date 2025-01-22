a224a318bab7e162a0b8980c19b8eb146fef8fd3
Pedro H Azevedo <100697662+pedrohazevedovm@users.noreply.github.com>
2025-01-22 15:22:28 -0300

    feat: iqa datasets classes

--- utils/distortions.py
+++ utils/distortions.py
@@ -308,3 +308,485 @@ def jpeg(x: torch.Tensor, quality: int) -> torch.Tensor:
     y = encode_jpeg(x.byte().cpu(), quality=quality)
     y = (decode_jpeg(y) / 255.).to(torch.float32)
     return y
+
+
+def white_noise(x: torch.Tensor, var: float, clip: bool = True, rounds: bool = False) -> torch.Tensor:
+    """
+    Adds Gaussian white noise to an input image tensor and optionally applies clipping and rounding.
+
+    Args:
+    x : torch.Tensor
+        Input image tensor with shape `(C, H, W)` or `(N, C, H, W)` where `C` is the number of channels.
+        Pixel values should be in the range `[0, 1]`.
+    var : float
+        Variance of the Gaussian white noise to be added.
+    clip : bool, optional
+        If `True`, clips the resulting tensor to the range `[0, 1]`. Default is `True`.
+    rounds : bool, optional
+        If `True`, rounds the resulting tensor to the nearest 8-bit integer (simulates quantization). Default is `False`.
+
+    Returns:
+    torch.Tensor
+        Output tensor with added Gaussian noise and optionally clipped or rounded values.
+        The output shape matches the input shape.
+    """
+    noise = torch.randn(*x.size(), dtype=x.dtype) * math.sqrt(var)
+
+    y = x + noise
+
+    if clip and rounds:
+        y = torch.clip((y * 255.0).round(), 0, 255) / 255.
+    elif clip:
+        y = torch.clip(y, 0, 1)
+    elif rounds:
+        y = (y * 255.0).round() / 255.
+    return y
+
+
+def white_noise_cc(x: torch.Tensor, var: float, clip: bool = True, rounds: bool = False) -> torch.Tensor:
+    """
+        Adds Gaussian white noise to an input image tensor in the YCbCr color space
+        and optionally applies clipping and rounding after converting back to RGB.
+
+        Args:
+        x : torch.Tensor
+            Input image tensor with shape `(C, H, W)` or `(N, C, H, W)` where `C` is the number of channels.
+            Pixel values should be in the range `[0, 1]`.
+        var : float
+            Variance of the Gaussian white noise to be added.
+        clip : bool, optional
+            If `True`, clips the resulting tensor to the range `[0, 1]`. Default is `True`.
+        rounds : bool, optional
+            If `True`, rounds the resulting tensor to the nearest 8-bit integer (simulates quantization). Default is `False`.
+
+        Returns:
+        torch.Tensor
+            Output tensor with added Gaussian noise, converted back to RGB, and optionally clipped or rounded.
+            The output shape matches the input shape.
+    """
+    noise = torch.randn(*x.size(), dtype=x.dtype) * math.sqrt(var)
+
+    ycbcr = kornia.color.rgb_to_ycbcr(x)
+    y = ycbcr + noise
+
+    y = kornia.color.ycbcr_to_rgb(y)
+
+    if clip and rounds:
+        y = torch.clip((y * 255.0).round(), 0, 255) / 255.
+    elif clip:
+        y = torch.clip(y, 0, 1)
+    elif rounds:
+        y = (y * 255.0).round() / 255.
+
+    return y
+
+
+def impulse_noise(x: torch.Tensor, d: float, s_vs_p: float = 0.5) -> torch.Tensor:
+    """
+      Adds impulse noise (salt-and-pepper noise) to an input image tensor.
+
+      Args:
+      x : torch.Tensor
+          Input image tensor with shape `(C, H, W)` where `C` is the number of channels.
+          Pixel values should be in the range `[0, 1]`.
+      d : float
+          Density of the impulse noise. This value determines the proportion of pixels
+          affected by salt-and-pepper noise. Should be in the range `[0, 1]`.
+      s_vs_p : float, optional
+          Salt-to-pepper ratio. Determines the proportion of affected pixels that become
+          "salt" (white, value = 1) vs "pepper" (black, value = 0). Default is `0.5` (equal salt and pepper).
+
+      Returns:
+      torch.Tensor
+          Output tensor with added salt-and-pepper noise. The shape matches the input shape.
+    """
+    num_sp = int(d * x.shape[0] * x.shape[1] * x.shape[2])
+
+    coords = np.concatenate((np.random.randint(0, 3, (num_sp, 1)),
+                             np.random.randint(0, x.shape[1], (num_sp, 1)),
+                             np.random.randint(0, x.shape[2], (num_sp, 1))), 1)
+
+    num_salt = int(s_vs_p * num_sp)
+
+    coords_salt = coords[:num_salt].transpose(1, 0)
+    coords_pepper = coords[num_salt:].transpose(1, 0)
+
+    x[coords_salt] = 1
+    x[coords_pepper] = 0
+
+    return x
+
+
+def multiplicative_noise(x: torch.Tensor, var: float) -> torch.Tensor:
+    """
+        Adds multiplicative noise to an input image tensor.
+
+        Args:
+        x : torch.Tensor
+            Input image tensor with shape `(C, H, W)` where `C` is the number of channels.
+            Pixel values should be in the range `[0, 1]`.
+        var : float
+            Variance of the multiplicative noise. Determines the intensity of the noise.
+
+        Returns:
+        torch.Tensor
+            Output tensor with added multiplicative noise. Pixel values are clipped to the range `[0, 1]`.
+    """
+    noise = torch.randn(*x.size(), dtype=x.dtype) * math.sqrt(var)
+    y = x + x * noise
+    y = torch.clip(y, 0, 1)
+    return y
+
+
+def brighten(x: torch.Tensor, amount: float) -> torch.Tensor:
+    """
+    Adjusts the brightness of an input image tensor by modifying its luminance and applying a tone curve.
+
+    Args:
+    x : torch.Tensor
+        Input image tensor with shape `(C, H, W)` where `C` is the number of channels.
+        Pixel values should be in the range `[0, 1]` and represent an RGB image.
+    amount : float
+        Brightness adjustment factor. Positive values increase brightness,
+        while negative values decrease brightness.
+
+    Returns:
+    torch.Tensor
+        Output image tensor with adjusted brightness. Pixel values remain in the range `[0, 1]`.
+    """
+    x = x[[2, 1, 0]]
+    lab = kornia.color.rgb_to_lab(x)
+
+    l = lab[0, ...] / 100.
+    l_ = curves(l, 0.5 + amount / 2)
+    lab[0, ...] = l_ * 100.
+
+    y = curves(x, 0.5 + amount / 2)
+
+    j = torch.clamp(kornia.color.lab_to_rgb(lab), 0, 1)
+
+    y = (2 * y + j) / 3
+
+    return y[[2, 1, 0]]
+
+
+def darken(x: torch.Tensor, amount: float, dolab: bool = False) -> torch.Tensor:
+    """
+       Reduces the brightness of an input image tensor by applying a tone curve and optionally adjusting luminance in LAB color space.
+
+       Args:
+       -----
+       x : torch.Tensor
+           Input image tensor with shape `(C, H, W)` where `C` is the number of channels.
+           Pixel values should be in the range `[0, 1]` and represent an RGB image.
+       amount : float
+           Darkness adjustment factor. Positive values increase the level of darkening.
+       dolab : bool, optional
+           If `True`, adjusts the luminance in the LAB color space in addition to applying the tone curve. Default is `False`.
+
+       Returns:
+       --------
+       torch.Tensor
+           Output image tensor with reduced brightness. Pixel values remain in the range `[0, 1]`.
+       """
+    x = x[[2, 1, 0], :, :]
+    lab = kornia.color.rgb_to_lab(x)
+    if dolab:
+        l = lab[0, ...] / 100.
+        l_ = curves(l, 0.5 + amount / 2)
+        lab[0, ...] = l_ * 100.
+
+    y = curves(x, 0.5 - amount / 2)
+
+    if dolab:
+        j = torch.clamp(kornia.color.lab_to_rgb(lab), 0, 1)
+        y = (2 * y + j) / 3
+
+    return y[[2, 1, 0]]
+
+
+def mean_shift(x: torch.Tensor, amount: float) -> torch.Tensor:
+    """
+    Adjusts the mean intensity of an image by adding a constant value to its pixel intensities.
+
+    Args:
+    -----
+    x : torch.Tensor
+        Input image tensor with shape `(C, H, W)` where `C` is the number of channels.
+        Pixel values should be in the range `[0, 1]` and represent an RGB image.
+    amount : float
+        The value to be added to the pixel intensities. Positive values brighten the image,
+        while negative values darken it.
+
+    Returns:
+    --------
+    torch.Tensor
+        Output image tensor with adjusted mean intensity. Pixel values are clamped to the range `[0, 1]`.
+    """
+    x = x[[2, 1, 0], :, :]
+
+    y = torch.clamp(x + amount, 0, 1)
+    return y[[2, 1, 0]]
+
+
+def jitter(x: torch.Tensor, amount: float) -> torch.Tensor:
+    """
+       Applies a jitter effect to an image by randomly displacing its pixels.
+
+       Args:
+       x : torch.Tensor
+           Input image tensor with shape `(C, H, W)` where `C` is the number of channels.
+           Pixel values should be in the range `[0, 1]` and represent an RGB image.
+       amount : float
+           The magnitude of pixel displacement. Larger values result in more noticeable jitter.
+
+       Returns:
+       torch.Tensor
+           Output image tensor with the applied jitter effect. Pixel values remain in the range `[0, 1]`.
+       """
+    y = imscatter(x, amount, 5)
+    return y
+
+
+def non_eccentricity_patch(x: torch.Tensor, pnum: int) -> torch.Tensor:
+    """
+     Applies a non-eccentricity patch effect by copying random patches of the image to nearby locations.
+
+     Args:
+     x : torch.Tensor
+         Input image tensor with shape `(C, H, W)` where `C` is the number of channels.
+         Pixel values should be in the range `[0, 1]`.
+     pnum : int
+         Number of patches to generate and apply.
+
+     Returns:
+     torch.Tensor
+         Output image tensor with the applied non-eccentricity patch effect. Pixel values remain in the range `[0, 1]`.
+
+     Notes:
+     - The function randomly selects patches of size `16x16` pixels from the input image and copies them
+       to nearby locations within a `16-pixel` radius.
+     - The original image content outside the patches remains unchanged.
+     """
+    y = x
+    patch_size = [16, 16]
+    radius = 16
+    h_min = radius
+    w_min = radius
+    c, h, w = x.shape
+
+    h_max = h - patch_size[0] - radius
+    w_max = w - patch_size[1] - radius
+
+    for i in range(pnum):
+        w_start = round(random.random() * (w_max - w_min)) + w_min
+        h_start = round(random.random() * (h_max - h_min)) + h_min
+        patch = y[:, h_start:h_start + patch_size[0], w_start:w_start + patch_size[0]]
+
+        rand_w_start = round((random.random() - 0.5) * radius + w_start)
+        rand_h_start = round((random.random() - 0.5) * radius + h_start)
+        y[:, rand_h_start:rand_h_start + patch_size[0], rand_w_start:rand_w_start + patch_size[0]] = patch
+
+    return y
+
+
+def pixelate(x: torch.Tensor, strength: float) -> torch.Tensor:
+    """
+    Applies a pixelation effect to the input image by resizing and then resizing it back.
+
+    Args:
+    x : torch.Tensor
+        Input image tensor with shape `(C, H, W)` where `C` is the number of channels.
+        Pixel values should be in the range `[0, 1]`.
+    strength : float
+        Strength of the pixelation effect. Higher values result in stronger pixelation.
+
+    Returns:
+    torch.Tensor
+        Output image tensor with the pixelation effect applied. Pixel values remain in the range `[0, 1]`.
+    """
+    z = 0.95 - strength ** 0.6
+    c, h, w = x.shape
+
+    ylo = kornia.geometry.transform.resize(x, (int(h * z), int(w * z)), 'nearest')
+    y = kornia.geometry.transform.resize(ylo, (h, w), 'nearest')
+
+    return y
+
+
+def quantization(x: torch.Tensor, levels: int) -> torch.Tensor:
+    """
+    Applies quantization to the input image by reducing the number of intensity levels.
+
+    Args:
+    x : torch.Tensor
+        Input image tensor with shape `(C, H, W)` where `C` is the number of channels.
+        Pixel values should be in the range `[0, 1]`.
+    levels : int
+        The number of intensity levels to quantize the image into. Must be between 1 and 256.
+
+    Returns:
+    torch.Tensor
+        The quantized image tensor with reduced intensity levels. Pixel values remain in the range `[0, 1]`.
+    """
+    image = kornia.color.rgb_to_grayscale(x) * 255
+    image = image.cpu().numpy()
+    num_classes = levels
+
+    # minimum variance thresholding
+    hist, bins = np.histogram(image, num_classes, [0, 255])
+
+    return_thresholds = np.zeros(num_classes - 1)
+    for i in range(num_classes - 1):
+        return_thresholds[i] = bins[i + 1]
+
+    # quantize image with thresholds
+    bins = torch.tensor([0] + return_thresholds.tolist() + [256])
+    bins = bins.type(torch.int)
+    image = torch.bucketize(x.contiguous() * 255., bins).to(torch.float32)
+    image = mapmm(image)
+    return image
+
+
+def color_block(x: torch.Tensor, pnum: int) -> torch.Tensor:
+    """
+    Adds color blocks to the input image by randomly placing patches of a uniform color.
+
+    Args:
+    x : torch.Tensor
+        Input image tensor with shape `(C, H, W)` where `C` is the number of channels.
+        The pixel values should be in the range `[0, 1]`.
+    pnum : int
+        The number of color blocks (patches) to add to the image.
+
+    Returns:
+    torch.Tensor
+        The image with added color blocks, with the same shape as the input image.
+    """
+    patch_size = [32, 32]
+
+    c, w, h = x.shape
+
+    y = x
+
+    h_max = h - patch_size[0]
+    w_max = w - patch_size[1]
+
+    for i in range(pnum):
+        color = np.random.random(3)
+        px = math.floor(random.random() * w_max)
+        py = math.floor(random.random() * h_max)
+        patch = torch.ones((3, patch_size[0], patch_size[1]))
+        for j in range(3):
+            patch[j, ...] *= color[j]
+        y[:, px:px + patch_size[0], py:py + patch_size[1]] = patch
+
+    return y
+
+
+def high_sharpen(x: torch.Tensor, amount: int, radius: int = 3) -> torch.Tensor:
+    """
+     Applies a high-pass sharpening filter to enhance the high-frequency details of an image.
+
+     Args:
+     x : torch.Tensor
+         Input image tensor in the format `(C, H, W)` where `C` is the number of channels (RGB),
+         with pixel values in the range `[0, 1]`.
+     amount : int
+         The intensity of sharpening to apply. A higher value results in stronger sharpening.
+     radius : int, optional (default=3)
+         The radius of the Gaussian blur used to create the sharpening filter. A larger radius
+         applies a broader blur before the sharpening step.
+
+     Returns:
+     torch.Tensor
+         The sharpened image tensor with the same shape as the input.
+     """
+    x = x[[2, 1, 0], ...]
+    lab = kornia.color.rgb_to_lab(x)
+    l = lab[0:1, ...].unsqueeze(0)
+
+    filt_radius = math.ceil(radius * 2)
+    fs = 2 * filt_radius + 1
+    h = fspecial('gaussian', (fs, fs), filt_radius)
+    h = torch.from_numpy(h).float()
+
+    sharp_filter = torch.zeros((fs, fs))
+    sharp_filter[filt_radius, filt_radius] = 1
+    sharp_filter = sharp_filter - h
+
+    sharp_filter *= amount
+    sharp_filter[filt_radius, filt_radius] += 1
+
+    l = filter2D(l, sharp_filter.unsqueeze(0))
+
+    lab[0, ...] = l
+
+    if len(lab.shape) == 3:
+        lab = lab.unsqueeze(0)
+
+    y = kornia.color.lab_to_rgb(lab)
+    y = y[:, [2, 1, 0]].squeeze(0)
+    return y
+
+
+def linear_contrast_change(x: torch.Tensor, amount: float) -> torch.Tensor:
+    """
+    Applies a linear contrast change to an image.
+
+    Args:
+    x : torch.Tensor
+        Input image tensor in the format `(C, H, W)` where `C` is the number of channels (RGB),
+        with pixel values in the range `[0, 1]`.
+    amount : float
+        The amount of contrast change to apply. A positive value increases contrast, while a negative value decreases it.
+
+    Returns:
+    torch.Tensor
+        The contrast-adjusted image tensor with the same shape as the input.
+    """
+    y = curves(x, [0.25 - amount / 4, 0.75 + amount / 4])
+    return y
+
+
+def non_linear_contrast_change(x: torch.Tensor, output_offset_value: float, output_central_value: float = 0.5,
+                               input_offset_value: float = 0.5, input_central_value: float = 0.5) -> torch.Tensor:
+    """
+      Applies a non-linear contrast adjustment to an image.
+
+      Args:
+      x : torch.Tensor
+          Input image tensor in the format `(C, H, W)` where `C` is the number of channels (e.g., RGB),
+          with pixel values in the range `[0, 1]`.
+      output_offset_value : float
+          The offset value for the output range, which determines the contrast intensity in the output image.
+      output_central_value : float, optional, default=0.5
+          The central value for the output range. This is where the output image's pixel values are centered.
+      input_offset_value : float, optional, default=0.5
+          The offset value for the input range. This defines the range of input values to be adjusted.
+      input_central_value : float, optional, default=0.5
+          The central value for the input range. This is where the input image's pixel values are centered.
+
+      Returns:
+      torch.Tensor
+          The contrast-adjusted image tensor with the same shape as the input.
+      """
+    low_in = input_central_value - input_offset_value
+    high_in = input_central_value + input_offset_value
+    low_out = output_central_value - output_offset_value
+    high_out = output_central_value + output_offset_value
+
+    # Clip the input image to the specified input range
+    x = np.clip(x, low_in, high_in)
+
+    # Calculate the slope and intercept of the linear transformation
+    slope = (high_out - low_out) / (high_in - low_in)
+    intercept = low_out - slope * low_in
+
+    # Apply the linear transformation to adjust the pixel values
+    y = slope * x + intercept
+
+    # Clip the adjusted image to the specified output range
+    y = np.clip(y, low_out, high_out)
+
+    return y
