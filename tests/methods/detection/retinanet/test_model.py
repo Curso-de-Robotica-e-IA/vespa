8254c6bfe4b629968c49b076c9c226f9e34a717c
jefferson-norberto2 <jmn@cin.ufpe.br>
2025-02-10 14:11:40 -0300

    REFACTOR: Folder structure pattern

--- tests/methods/detection/retinanet/test_model.py
+++ tests/methods/detection/retinanet/test_model.py
@@ -66,3 +66,70 @@ def test_model_train_loop_cpu(
     dataset = yolo_dataset_train_retina
 
     model.fit(dataset, 1, 1, 'cpu')
+
+
+def test_model_sketch_list(retina_sketch_fixture, tensor_image_fixture):
+    model = retina_sketch_fixture
+    image = tensor_image_fixture
+    model.eval()
+    results = model(image)
+
+    assert isinstance(results, list)
+
+
+def test_model_sketch_dict(retina_sketch_fixture, tensor_image_fixture):
+    model = retina_sketch_fixture
+    image = tensor_image_fixture
+    model.eval()
+    results = model(image)
+
+    for result in results:
+        assert isinstance(result, dict)
+
+
+def test_model_sketch_boxes(retina_sketch_fixture, tensor_image_fixture):
+    model = retina_sketch_fixture
+    image = tensor_image_fixture
+    model.eval()
+    results = model(image)
+
+    for result in results:
+        assert isinstance(result['boxes'], Tensor)
+
+
+def test_model_sketch_scores(retina_sketch_fixture, tensor_image_fixture):
+    model = retina_sketch_fixture
+    image = tensor_image_fixture
+    model.eval()
+    results = model(image)
+
+    for result in results:
+        assert isinstance(result['scores'], Tensor)
+
+
+def test_model_sketch_labels(retina_sketch_fixture, tensor_image_fixture):
+    model = retina_sketch_fixture
+    image = tensor_image_fixture
+    model.eval()
+    results = model(image)
+
+    for result in results:
+        assert isinstance(result['labels'], Tensor)
+
+
+def test_model_sketch_train_loop_gpu(
+    retina_pretrained_fixture, yolo_dataset_train_retina
+):
+    model = retina_pretrained_fixture
+    dataset = yolo_dataset_train_retina
+
+    model.fit(dataset, 4, 4, 0)
+
+
+def test_model_sketch_train_loop_cpu(
+    retina_pretrained_fixture, yolo_dataset_train_retina
+):
+    model = retina_pretrained_fixture
+    dataset = yolo_dataset_train_retina
+
+    model.fit(dataset, 1, 1, 'cpu')
