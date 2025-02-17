705dd8e304e9fa530df575f0345154c71efc6785
Matheus-Hopper <mhjc@cin.ufpe.br>
2025-02-17 15:16:24 -0300

    REFACTOR: refactoring tranformation function

--- tests/methods/detection/rcnn/test_model.py
+++ tests/methods/detection/rcnn/test_model.py
@@ -2,49 +2,39 @@ from torch import Tensor
 
 
 def test_model_list(rcnn_pretrained_fixture, tensor_image_fixture):
-    model = rcnn_pretrained_fixture
-    image = tensor_image_fixture
-    model.eval()
-    results = model(image)
+    rcnn_pretrained_fixture.eval()
+    results = rcnn_pretrained_fixture(tensor_image_fixture)
 
     assert isinstance(results, list)
 
 
 def test_model_dict(rcnn_pretrained_fixture, tensor_image_fixture):
-    model = rcnn_pretrained_fixture
-    image = tensor_image_fixture
-    model.eval()
-    results = model(image)
+    rcnn_pretrained_fixture.eval()
+    results = rcnn_pretrained_fixture(tensor_image_fixture)
 
     for result in results:
         assert isinstance(result, dict)
 
 
 def test_model_boxes(rcnn_pretrained_fixture, tensor_image_fixture):
-    model = rcnn_pretrained_fixture
-    image = tensor_image_fixture
-    model.eval()
-    results = model(image)
+    rcnn_pretrained_fixture.eval()
+    results = rcnn_pretrained_fixture(tensor_image_fixture)
 
     for result in results:
         assert isinstance(result['boxes'], Tensor)
 
 
 def test_model_scores(rcnn_pretrained_fixture, tensor_image_fixture):
-    model = rcnn_pretrained_fixture
-    image = tensor_image_fixture
-    model.eval()
-    results = model(image)
+    rcnn_pretrained_fixture.eval()
+    results = rcnn_pretrained_fixture(tensor_image_fixture)
 
     for result in results:
         assert isinstance(result['scores'], Tensor)
 
 
 def test_model_labels(rcnn_pretrained_fixture, tensor_image_fixture):
-    model = rcnn_pretrained_fixture
-    image = tensor_image_fixture
-    model.eval()
-    results = model(image)
+    rcnn_pretrained_fixture.eval()
+    results = rcnn_pretrained_fixture(tensor_image_fixture)
 
     for result in results:
         assert isinstance(result['labels'], Tensor)
@@ -53,65 +43,49 @@ def test_model_labels(rcnn_pretrained_fixture, tensor_image_fixture):
 def test_model_train_loop_gpu(
     rcnn_pretrained_fixture, yolo_dataset_train_rcnn
 ):
-    model = rcnn_pretrained_fixture
-    dataset = yolo_dataset_train_rcnn
-
-    model.fit(dataset, 4, 4, 0)
+    rcnn_pretrained_fixture.fit(yolo_dataset_train_rcnn, 4, 4, 0)
 
 
 def test_model_train_loop_cpu(
     rcnn_pretrained_fixture, yolo_dataset_train_rcnn
 ):
-    model = rcnn_pretrained_fixture
-    dataset = yolo_dataset_train_rcnn
-
-    model.fit(dataset, 1, 1, 'cpu')
+    rcnn_pretrained_fixture.fit(yolo_dataset_train_rcnn, 1, 1, 'cpu')
 
 
 def test_model_sketch_list(rcnn_sketch_fixture, tensor_image_fixture):
-    model = rcnn_sketch_fixture
-    image = tensor_image_fixture
-    model.eval()
-    results = model(image)
+    rcnn_sketch_fixture.eval()
+    results = rcnn_sketch_fixture(tensor_image_fixture)
 
     assert isinstance(results, list)
 
 
 def test_model_sketch_dict(rcnn_sketch_fixture, tensor_image_fixture):
-    model = rcnn_sketch_fixture
-    image = tensor_image_fixture
-    model.eval()
-    results = model(image)
+    rcnn_sketch_fixture.eval()
+    results = rcnn_sketch_fixture(tensor_image_fixture)
 
     for result in results:
         assert isinstance(result, dict)
 
 
 def test_model_sketch_boxes(rcnn_sketch_fixture, tensor_image_fixture):
-    model = rcnn_sketch_fixture
-    image = tensor_image_fixture
-    model.eval()
-    results = model(image)
+    rcnn_sketch_fixture.eval()
+    results = rcnn_sketch_fixture(tensor_image_fixture)
 
     for result in results:
         assert isinstance(result['boxes'], Tensor)
 
 
 def test_model_sketch_scores(rcnn_sketch_fixture, tensor_image_fixture):
-    model = rcnn_sketch_fixture
-    image = tensor_image_fixture
-    model.eval()
-    results = model(image)
+    rcnn_sketch_fixture.eval()
+    results = rcnn_sketch_fixture(tensor_image_fixture)
 
     for result in results:
         assert isinstance(result['scores'], Tensor)
 
 
 def test_model_sketch_labels(rcnn_sketch_fixture, tensor_image_fixture):
-    model = rcnn_sketch_fixture
-    image = tensor_image_fixture
-    model.eval()
-    results = model(image)
+    rcnn_sketch_fixture.eval()
+    results = rcnn_sketch_fixture(tensor_image_fixture)
 
     for result in results:
         assert isinstance(result['labels'], Tensor)
@@ -120,16 +94,10 @@ def test_model_sketch_labels(rcnn_sketch_fixture, tensor_image_fixture):
 def test_model_sketch_train_loop_gpu(
     rcnn_sketch_fixture, yolo_dataset_train_rcnn
 ):
-    model = rcnn_sketch_fixture
-    dataset = yolo_dataset_train_rcnn
-
-    model.fit(dataset, 4, 4, 0)
+    rcnn_sketch_fixture.fit(yolo_dataset_train_rcnn, 4, 4, 0)
 
 
 def test_model_sketch_train_loop_cpu(
     rcnn_sketch_fixture, yolo_dataset_train_rcnn
 ):
-    model = rcnn_sketch_fixture
-    dataset = yolo_dataset_train_rcnn
-
-    model.fit(dataset, 1, 1, 'cpu')
+    rcnn_sketch_fixture.fit(yolo_dataset_train_rcnn, 1, 1, 'cpu')
